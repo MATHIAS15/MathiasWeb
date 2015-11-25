0a02cc6bd347f018a939edac795ab61cb0e02260
Forestier Hugo <Forestier Hugo>
2015-11-25 16:57:53 +0100

    Initial commit

---
+++ MathiasWeb3/Controllers/SettingsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using COREMODEL;
+
+namespace MathiasWeb3.Controllers
+{
+    public class SettingsController : Controller
+    {
+        // GET: Settings
+        public ActionResult Index()
+        {
+            USERS user = new USERS();
+
+            return View();
+        }
+    }
+}
---
+++ MathiasWeb3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MathiasWeb3.Startup))]
+namespace MathiasWeb3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
